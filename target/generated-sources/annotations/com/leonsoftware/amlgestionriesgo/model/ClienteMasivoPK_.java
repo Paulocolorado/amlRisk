4a077375eecf1eb0aeeae791b84c603721406e5a
LeonSoft <33138932+LeonSoft@users.noreply.github.com>
2018-01-09 16:47:00 -0500

    version 2.0  cambio de version
    - se modifica directorio y version informativa

--- target/generated-sources/annotations/com/leonsoftware/amlgestionriesgo/model/ClienteMasivoPK_.java
+++ target/generated-sources/annotations/com/leonsoftware/amlgestionriesgo/model/ClienteMasivoPK_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-03T16:24:35")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-01-09T16:36:33")
 @StaticMetamodel(ClienteMasivoPK.class)
 public class ClienteMasivoPK_ { 
 
