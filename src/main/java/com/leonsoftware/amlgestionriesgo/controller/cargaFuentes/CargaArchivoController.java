bb0c70cd00b2e6fbf084f018d8136f0dbd217ff9
LeonSoft <33138932+LeonSoft@users.noreply.github.com>
2018-01-09 21:40:44 -0500

    Version 2.1.
    - Se incluye validacion para fechas de registro en fuente externa nulos

--- src/main/java/com/leonsoftware/amlgestionriesgo/controller/cargaFuentes/CargaArchivoController.java
+++ src/main/java/com/leonsoftware/amlgestionriesgo/controller/cargaFuentes/CargaArchivoController.java
@@ -327,8 +327,11 @@ public class CargaArchivoController implements Serializable{
                 listaRestriccion.getListaRestriccionPK().setListaIdRegistro(sigIdLista);
                 listaRestriccion.getListaRestriccionPK().setTbArchivoFuenteIdArchivoFuente(idArchivo);
                 listaRestriccion.setListaPrimerNombre(campo[0]);
-                listaRestriccion.setListaUltimoNombre(campo[1]);
-                fechaReporte.setTime(sdf.parse(campo[2]));
+                listaRestriccion.setListaUltimoNombre(campo[0]);
+                if(!campo[2].isEmpty()){
+                    fechaReporte.setTime(sdf.parse(campo[2]));
+                    
+                }
                 listaRestriccion.setListaFechaReporte(fechaReporte.getTime());
                 listaRestriccion.setListaObservacion(campo[3]);
                 listaRestriccionCollection.add(listaRestriccion); 
